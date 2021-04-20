69dc678c7d2cc49b60e668ad6e1b8c0f8b5263f4
Hasib787 <mr.hasib45@gmail.com>
2021-04-20 07:06:43 +0600

    connected database on mongodb

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fg2cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+client.connect(err => {
+    const booksCollection = client.db("drivingSchool").collection("programs");
+    console.log("database connected")
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 5000)
