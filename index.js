391e488050ff418d54588613e24a26e1004d9408
Hasib787 <mr.hasib45@gmail.com>
2021-04-24 08:41:29 +0600

    final

--- index.js
+++ index.js
@@ -9,31 +9,32 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fg2cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 
 const serviceAccount = require("./cab-driving-school-firebase-adminsdk-g12f2-860d3a39cb.json");
 
-admin.initializeApp({ 
-  credential: admin.credential.cert(serviceAccount)
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
 
 
 client.connect(err => {
     const programsCollection = client.db("drivingSchool").collection("programs");
 
-        app.get('/programs',(req, res) => {
-            programsCollection.find()
+    app.get('/programs', (req, res) => {
+        programsCollection.find()
             .toArray((err, items) => {
                 res.send(items);
             })
-        })
-        //For add programs
-        app.post('/addPrograms', (req, res) => {
-            const newprogram = req.body;
-            console.log('adding new event', newprogram);
-            programsCollection.insertOne(newprogram)
+    })
+    //For add programs
+    app.post('/addPrograms', (req, res) => {
+        const newprogram = req.body;
+        console.log('adding new event', newprogram);
+        programsCollection.insertOne(newprogram)
             .then(result => {
                 console.log('inserted count', result.insertedCount)
                 res.send(result.insertedCount > 0)
@@ -41,8 +42,8 @@ client.connect(err => {
     })
 
     app.get('/programItem/:bookid', (req, res) => {
-	    const bookid = ObjectID(req.params.bookid);
-        programsCollection.find({_id: bookid})
+        const bookid = ObjectID(req.params.bookid);
+        programsCollection.find({ _id: bookid })
             .toArray((err, documents) => {
                 res.send(documents[0]);
             })
@@ -50,17 +51,17 @@ client.connect(err => {
 
     app.post('/programItemByIds', (req, res) => {
         const programitemIds = req.body;
-        programsCollection.find({_id: { $in: programitemIds}})
-        .toArray((err, documents)=>{
-            res.send(documents)
-        })
-    }) 
+        programsCollection.find({ _id: { $in: programitemIds } })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
 
-    app.delete('/deleteService/:id', (req, res)=> {
+    app.delete('/deleteService/:id', (req, res) => {
         const id = ObjectID(req.params.id);
-        console.log("delete this",id);
-        booksCollection.findOneAndDelete({_id: id})
-        .then(documents => res.send(documents.deletedCount > 0))
+        console.log("delete this", id);
+        booksCollection.findOneAndDelete({ _id: id })
+            .then(documents => res.send(documents.deletedCount > 0))
     })
 });
 
@@ -110,7 +111,7 @@ client.connect(err => {
             })
         console.log(newAdmin);
     })
-//Amin Collection 
+    //Amin Collection 
     app.get('/admins', (req, res) => {
         adminCollection.find({})
             .toArray((err, documents) => {
@@ -133,23 +134,23 @@ client.connect(err => {
 client.connect(err => {
     const reviewCollection = client.db("drivingSchool").collection("reviews");
 
-    app.get('/reviews',(req, res) => {
+    app.get('/reviews', (req, res) => {
         reviewCollection.find()
-        .toArray((err, items) => {
-            res.send(items);
-        })
+            .toArray((err, items) => {
+                res.send(items);
+            })
     })
 
-     //For add reviews
-     app.post('/addReview', (req, res) => {
+    //For add reviews
+    app.post('/addReview', (req, res) => {
         const newreview = req.body;
         console.log('adding new event', newreview);
         reviewCollection.insertOne(newreview)
-        .then(result => {
-            console.log('inserted count', result.insertedCount)
-            res.send(result.insertedCount > 0)
-        })
-})
+            .then(result => {
+                console.log('inserted count', result.insertedCount)
+                res.send(result.insertedCount > 0)
+            })
+    })
 });
 
 app.get('/', (req, res) => {
